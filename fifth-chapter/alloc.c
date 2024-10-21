241574a4ef8907214c99bffb4e042561c5a8ebf6
colbird <coldbird@yeah.net>
2024-10-21 09:22:44 +0800

    10.21	sort函数

--- fifth-chapter/alloc.c
+++ fifth-chapter/alloc.c
@@ -7,10 +7,10 @@ void afree(char *);
 int main(){
 	char *name1;
 	char *name2;
-	name1 = alloc(strlen("dupengjun") + 1);
-	name2 = alloc(strlen("dupengxi") + 1);
-	name1 = "dupengjun";
-	name2 = "dupengxi";
+	name1 = alloc(strlen("xxx") + 1);
+	name2 = alloc(strlen("look") + 1);
+	name1 = "xxx";
+	name2 = "look";
 	printf ("%s\n", name1);
 	printf ("%s\n", name2);
 	afree(name1);
---
+++ fifth-chapter/sort.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000		/*  进行排序的最大文本行数  */
+
+char *lineptr[MAXLINES];		/*  指向文本行的指针数组  */
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lienptr[], int nlines);
+
+void nqsort(char *lineptr[], int left, int right);
+
+/*  对输入的文本进行排序  */
+
+int main(){
+	int nlines;
+
+	if ((nlines = readlines(lineptr, MAXLINES)) >= 0){
+		nqsort(lineptr, 0, nlines - 1);
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("error: input: input too big to sort\n");
+		return 1;
+	}
+}
+
+
+#define MAXLEN 1000	/*  每个输入文本行的最大长度  */
+int ngetline(char *, int);
+char *alloc(int);
+
+/*  readlines函数: 读取输入行  */
+int readlines(char *lineptr[], int maxlines){
+	int len, nlines;
+	char *p, line[MAXLINES];
+	nlines = 0;
+	while ((len = ngetline(line, MAXLEN)) > 0)
+		if (nlines >= maxlines || (p = alloc(len)) == NULL)
+			return -1;
+		else {
+			line[len-1] = '\0';	/*  删除换行符  */
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+	return nlines;
+}
+int ngetline(char *s, int manlen){
+	int i = 0, c;
+	while ((c = getchar()) != EOF && c != '\n')
+		*(s + i++) = c;
+	if (c == '\n')
+		*(s + i++) = c;
+	return i;
+}
+
+#include <stdlib.h>
+
+char *alloc(int i){
+	return (char *)malloc(i);
+}
+
+void writelines(char *lineptr[], int nlines){
+	int i;
+
+	while (nlines-- > 0)
+		printf("%s\n", *lineptr++);
+}
+
+/*  nqsort函数: 按递增顺序对v[left]---v[right]进行排序  */
+void nqsort(char *v[], int left, int right){
+	int i, last;
+	void swap(char *v[], int i, int j);
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right)/2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+		if (strcmp(v[i], v[last]) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	nqsort(v, left, last-1);
+	nqsort(v, left+1, right);
+}
+
+/*  swap函数: 交换v[i]和v[j]  */
+void swap(char *v[], int i, int j){
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
